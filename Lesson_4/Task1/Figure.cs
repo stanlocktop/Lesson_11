c05a70e921d92710765aa7a1faffdb750f67ad8d
stanlocktop <wlad-test@ukr.net>
2023-10-08 20:58:05 +0300

    update project

---
+++ Lesson_4/Task1/Figure.cs
@@ -0,0 +1,63 @@
+using System;
+
+// Базовий клас Фігура
+class Figure
+{
+    // Поля для координат точки початку фігури (X та Y)
+    public int X { get; }
+    public int Y { get; }
+
+    // Конструктор класу Фігура
+    public Figure(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // Метод для виведення інформації про фігуру
+    public virtual void Display()
+    {
+        Console.WriteLine($"Фігура розташована в точці ({X}, {Y})");
+    }
+}
+
+// Похідний клас Коло
+class Circle : Figure
+{
+    // Додаткове поле для радіуса
+    public double Radius { get; }
+
+    // Конструктор класу Коло
+    public Circle(int x, int y, double radius) : base(x, y)
+    {
+        Radius = radius;
+    }
+
+    // Перевизначений метод для виведення інформації про коло
+    public override void Display()
+    {
+        Console.WriteLine($"Коло розташоване в точці ({X}, {Y}) з радіусом {Radius}");
+    }
+}
+
+// Похідний клас Прямокутник
+class Rectangle : Figure
+{
+    // Додаткові поля для ширини та висоти
+    public double Width { get; }
+    public double Height { get; }
+
+    // Конструктор класу Прямокутник
+    public Rectangle(int x, int y, double width, double height) : base(x, y)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    // Перевизначений метод для виведення інформації про прямокутник
+    public override void Display()
+    {
+        Console.WriteLine($"Прямокутник розташований в точці ({X}, {Y}) з шириною {Width} і висотою {Height}");
+        Console.ReadKey();
+    }
+}
---
+++ Lesson_4/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Створюємо об'єкти класу Коло та Прямокутник
+        Circle circle = new Circle(5, 5, 3.5);
+        Rectangle rectangle = new Rectangle(10, 10, 4.0, 6.0);
+
+        // Виводимо інформацію про фігури
+        circle.Display();
+        rectangle.Display();
+        Console.ReadKey();
+    }
+}
