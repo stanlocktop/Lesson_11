4447a66bfc65921e3da2a6bbe88c3ab967fee30b
stanlocktop <wlad-test@ukr.net>
2023-10-10 20:26:36 +0300

    finish

--- Lesson_11/Task1/Program.cs
+++ Lesson_11/Task1/Program.cs
@@ -3,11 +3,9 @@
     static void Main(string[] args)
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
-        // Створюємо об'єкти класу Коло та Прямокутник
         Circle circle = new Circle(5, 5, 3.5);
         Rectangle rectangle = new Rectangle(10, 10, 4.0, 6.0);
 
-        // Виводимо інформацію про фігури
         circle.Display();
         rectangle.Display();
         Console.ReadKey();
